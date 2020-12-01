2623e5f0eee16baf61e53c55487c5784ccb42959
Jered Odegard <j@jeredodegard.com>
2020-12-01 09:53:10 -0800

    Added tailwind and started styling podcast page.

--- pages/podcast.js
+++ pages/podcast.js
@@ -1,10 +1,7 @@
-// Build time function
-export async function getServerSideProps() {
-  // const res = await fetch("https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Ffeeds.transistor.fm%2Fjered-o-show");
-  // const episodes = await res.json();
+import Head from "next/head";
 
-  //const res = await fetch("https://feeds.transistor.fm/jered-o-show");
-  // const xml = await res.text();
+export async function getStaticProps() {
+  // Podcast RSS Feed
   const Parser = require("rss-parser");
   const parser = new Parser();
   const podcast = await parser.parseURL("https://feeds.transistor.fm/jered-o-show");
@@ -16,19 +13,37 @@ export async function getServerSideProps() {
 
 export default function Podcast({ podcast }) {
   return (
-    <>
-      <h1>Podcast episodes</h1>
-      {console.log(podcast)}
-      <a href={podcast.feedUrl}>{podcast.title}</a>
-      <ul>
+    <div className="p-6">
+      {console.dir(podcast)}
+      <Head>
+        <title>Podcast - Jered Odegard</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <a href={podcast.feedUrl} title={podcast.title}><img className="w-28 h-28" src={podcast.image.url} title={podcast.image.title} /></a>
+      <h1 className="text-3xl font-extrabold">{podcast.title}</h1>
+      <p className="text-xs w-80 text-gray-500 pt-2">{podcast.description}</p>
+      <div className="space-y-4 pt-6 pb-6">
         {podcast.items.map((episode, index) => (
-          <li key={index}>
-            <img src={episode.itunes.image} width="100px" height="100px" />
-            <h2><a href={episode.link}>{episode.title}</a></h2>{episode.content}&nbsp;
-            <a href={episode.link}>Listen here.</a>
-          </li>
+          <div className="flex items-center w-auto md:w-4/5 md:mx-auto h-28 space-x-4 rounded shadow-md p-4 overflow-hidden border border-gray-200" key={index}>
+            <div className="flex-shrink-0">
+              <a href={episode.link}>
+                <img src={episode.itunes.image} alt={episode.title} className="h-20 w-20 rounded" />
+              </a>
+            </div>
+            <div className="overflow-ellipsis overflow-hidden">
+              <div className="font-medium text-base">
+                <a href={episode.link}>{episode.title}</a>
+              </div>
+              <div className ="text-sm text-gray-500 truncate">
+                {episode.content}
+              </div>
+            </div>
+            <div className="text-purple-600 text-sm font-medium pr-2 pl-2 hover:text-purple-700">
+              <a href={episode.link}>Listen</a>
+            </div>
+          </div>
         ))}
-      </ul>
-    </>
+      </div>
+    </div>
   );
 }
