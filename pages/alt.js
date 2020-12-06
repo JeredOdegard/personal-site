1fb08db59d3e3e7aaca26507d5be2c35f5a90b98
Jered Odegard <j@jeredodegard.com>
2020-12-05 21:30:37 -0800

    Made things yellow.

--- pages/alt.js
+++ pages/alt.js
@@ -10,8 +10,8 @@ function Blog() {
       </Head>
 
       <div className="absolute z-0 w-full h-full">
-        <div className="bg-yellow-100 h-0"></div>
-        <svg className="fill-current text-yellow-100" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1920 744">
+        <div className="bg-yellow-100 h-96 lg:h-72 xl:h-0"></div>
+        <svg className="fill-current text-yellow-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1920 744">
           <path d="M0 0h1920v654.395c-179.423-73.807-402.81-73.807-670.16 0-164.619 45.447-467.454 141.64-810.272 0C294.326 594.387 147.803 624.185 0 743.787V0z" fill-rule="evenodd"/>
         </svg>
       </div>
@@ -19,7 +19,7 @@ function Blog() {
       <div className="p-24 pt-8 pb-4 z-10 relative">
 
         <div className="flex justify-between">
-          <nav className="text-gray-400 flex justify-between gap-x-8 font-medium">
+          <nav className="text-gray-500 flex justify-between gap-x-8 font-medium">
             <div className="h-20 w-16 bg-gray-900 flex items-center justify-center fill-current text-white relative -top-8 rounded-b-md">
               <svg className="h-8 w-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
                 <path d="M5.384 31.168l3.348-3.636c.936.72 2.082 1.26 3.438 1.62 1.356.36 2.778.54 4.266.54 2.016 0 3.888-.324 5.616-.972 1.728-.648 3.228-1.566 4.5-2.754 1.272-1.188 2.268-2.622 2.988-4.302.72-1.68 1.08-3.54 1.08-5.58 0-3.48-1.056-6.384-3.168-8.712l3.204-3.456L27.272 1l-3.24 3.672a13.895 13.895 0 00-3.51-1.422c-1.26-.324-2.61-.486-4.05-.486-2.016 0-3.894.312-5.634.936-1.74.624-3.24 1.512-4.5 2.664-1.26 1.152-2.25 2.55-2.97 4.194-.72 1.644-1.08 3.486-1.08 5.526 0 1.704.258 3.282.774 4.734a11.878 11.878 0 002.25 3.87L2 28.288l3.384 2.88zm5.868-12.744c-.216-.528-.324-1.272-.324-2.232 0-.792.114-1.548.342-2.268.228-.72.57-1.356 1.026-1.908a5.054 5.054 0 011.728-1.332c.696-.336 1.5-.504 2.412-.504.816 0 1.5.108 2.052.324l-7.236 7.92zm5.112 3.672c-.384 0-.762-.024-1.134-.072a3.343 3.343 0 01-.954-.252l7.236-7.92c.24.6.36 1.356.36 2.268 0 .792-.114 1.548-.342 2.268a5.631 5.631 0 01-1.026 1.908 4.867 4.867 0 01-1.728 1.314c-.696.324-1.5.486-2.412.486z" fillRule="nonzero"/>
@@ -54,7 +54,7 @@ function Blog() {
               <h2 className="text-4xl sm:text-5xl lg:text-7xl xl:text-8xl font-extrabold text-gray-900 mb-8 font-serif">Make your growth easy.</h2>
               <p className="text-lg sm:text-2xl font-medium sm:leading-10 mb-6 text-gray-500 max-w-screen">I help high performing professionals and executives at companies like <span className="font-bold text-gray-700">Amazon</span>,  <span className="font-bold text-gray-700">Facebook</span>, and <span className="font-bold text-gray-700">Microsoft</span> achieve their goals. I'm confident I can accelerate you towards where you want to be.</p>
                 <a href="#">
-                  <button className="px-8 py-4 bg-yellow-100 text-lg font-bold text-yellow-600 rounded mt-2 lg:mt-4 hover:bg-yellow-200">What is coaching?</button>
+                  <button className="px-8 py-4 bg-yellow-200 text-lg font-bold text-yellow-600 rounded mt-2 lg:mt-4 hover:bg-yellow-300">What is coaching?</button>
                 </a>
             </div>
             <div className="hidden xl:grid col-span-full mt-10 xl:mt-0 xl:col-span-4 mt-0">
